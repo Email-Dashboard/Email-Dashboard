32289866a9e53be2d03f1652af3b7a89955b83fe
Sadik Ay <sadikay91@gmail.com>
2017-12-21 13:55:53 +0300

    fix tld

--- config/session_store.rb
+++ config/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_notification_#{Rails.env}", domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: "_notification_#{Rails.env}", domain: :all, tld_length: 3
