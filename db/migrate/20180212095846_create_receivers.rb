38ce85339b88637b6f041e9ac4ed020ec1c91584
Muhammet <muhammet@mojilala.com>
2018-02-12 18:13:24 +0000

    Update 20180212095846_create_receivers.rb

--- db/migrate/20180212095846_create_receivers.rb
+++ db/migrate/20180212095846_create_receivers.rb
@@ -2,8 +2,8 @@ class CreateReceivers < ActiveRecord::Migration[5.1]
   def change
     create_table :receivers do |t|
       t.references :activity, foreign_key: true
-      t.string :email
-      t.string :phone
+      t.string :email, index: true
+      t.string :phone, index: true
 
       t.timestamps
     end
