43f944432b1799a591f318499371af1a027eb776
Sadik Ay <sadikay91@gmail.com>
2018-02-23 22:42:32 +0300

    add turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ import "./../src/application.css"
 const Rails = require('rails-ujs');
 Rails.start();
 
+import Turbolinks from "turbolinks";
+Turbolinks.start();
+
 import { Application } from 'stimulus'
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
